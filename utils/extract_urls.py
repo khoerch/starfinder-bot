7803d0c5cfe0e94d903d7b554632da6236f77357
Kevin Hoercher <khoerch@gmail.com>
2023-05-03 16:34:50 -0400

    Add file to extract absolute urls from web domain

---
+++ utils/extract_urls.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin, parse_qs
+
+# URL of the main site to scrape
+main_url = 'https://www.aonsrd.com/'
+filename = '../data/urls/starfinder.txt'
+
+# Set of already checked URLs to avoid duplicates
+checked_urls = set()
+
+def extract_params(url):
+    """Extracts all possible paths, queries, and params from a URL."""
+    parsed_url = urlparse(url)
+    paths = parsed_url.path.strip('/').split('/')
+    queries = parse_qs(parsed_url.query)
+    params = set()
+    for path in paths:
+        params.add(path)
+        for query, values in queries.items():
+            params.add('{}={}'.format(query, values[0]))
+    return params
+
+def check_links(url):
+    """Recursively checks all links on a given URL with the same domain."""
+    global checked_urls
+    # Make a GET request to the URL and get the HTML content
+    response = requests.get(url)
+    html_content = response.content
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+    # Find all the links on the page
+    links = soup.find_all('a')
+    # Check each link on the page and append urls to txt file
+    with open(filename, 'a') as f:
+        for link in links:
+            href = link.get('href')
+            if href and not href.startswith('mailto:'):
+                # Construct the absolute URL for the link
+                abs_url = urljoin(url, href)
+                # Parse the absolute URL to check if it belongs to the same domain
+                parsed_url = urlparse(abs_url)
+                if parsed_url.netloc == urlparse(main_url).netloc:
+                    # Check if the absolute URL has already been checked to avoid duplicates
+                    if abs_url not in checked_urls:
+                        # Add the absolute URL to the set of checked URLs and append to file
+                        checked_urls.add(abs_url)
+                        f.write(abs_url + '\n')
+                        # Print the absolute URL
+                        print(abs_url)
+                        # Recursively check all links on the absolute URL
+                        check_links(abs_url)
+
+# Check all links on the main site
+check_links(main_url)
+
+print('')
+print('---')
+print('Finished!!')
